3b83aa70ed33d97d53b42834c4c1d474aab7f26c
Brian Stocking <brianm.stocking@gmail.com>
2015-02-24 20:17:49 -0700

    done with everything but black diamond

---
+++ js/mainController.js
@@ -0,0 +1,24 @@
+var app = angular.module('sounder');
+
+app.controller('mainController', function($scope, soundService, $sce){
+
+    $scope.getSongs = function(){
+        soundService.getSongs($scope.search)
+            .then(function(data){
+                $scope.userData = data.data
+                $scope.search = ''
+
+            });
+    };
+
+    $scope.play = function(track_url){
+        SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+            $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+        });
+
+
+
+    }
+
+    $scope.getSongs();
+});
---
+++ js/soundService.js
@@ -0,0 +1,15 @@
+var app = angular.module('sounder');
+
+app.service('soundService', function($http){
+
+    this.getSongs = function(username){
+
+        return $http({
+
+                method: 'GET',
+                url: 'http://api.soundcloud.com/users/' + username + '/tracks.json?client_id=bda4ada8694db06efcac9cf97b872b3e'
+            })
+
+    }
+
+});
